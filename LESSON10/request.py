1c757effd8311382ff02c83840108176722f0961
YaroslavPich <pich.yaroslav@gmail.com>
2024-12-28 14:02:19 +0200

    lesson10_add_md

--- LESSON10/request.py
+++ LESSON10/request.py
@@ -1,6 +1,12 @@
+import json
+
 import requests
 
-web_site = 'https://jsonplaceholder.typicode.com/'
+web_site = 'https://jsonplaceholder.typicode.com/posts'
 
-response = requests.get(url=web_site)
-print(response.text)
+print('_______________GET__________________')
+response = requests.get(url=web_site + '/1')
+data = response.json()
+print(json.dumps(data, indent=4))
+print('_______________POST__________________')
+
